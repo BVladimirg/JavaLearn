adc8688e9c502b447689a36b94d931f6c2eb531d
VladimirBG <bukharkinv@gmail.com>
2016-10-17 17:32:16 +0300

    l18.l03.t04 is ready

--- JavaRushHomeWork/src/com/javarush/test/level18/lesson03/task04/Solution.java
+++ JavaRushHomeWork/src/com/javarush/test/level18/lesson03/task04/Solution.java
@@ -1,15 +1,47 @@
 package com.javarush.test.level18.lesson03.task04;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
-
-/* Самые редкие байты
-Ввести с консоли имя файла
-Найти байт или байты с минимальным количеством повторов
-Вывести их на экран через пробел
-Закрыть поток ввода-вывода
-*/
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+
+        FileInputStream inputStream = new FileInputStream(fileName);
+        //return HashMap with all file's bytes and counts of that bytes
+        HashMap<Integer, Integer> byteCount = new HashMap<Integer, Integer>();
+
+        int maxCount = 0;
+        while (inputStream.available() > 0) {
+            int byteKey = inputStream.read();
+            int count = 1;
+            if (byteCount.containsKey(byteKey)) {
+                count = byteCount.get(byteKey) + 1;
+                if (maxCount < count) maxCount = count;
+            }
+            byteCount.put(byteKey, count);
+        }
+        inputStream.close();
+
+        int minCount = maxCount;
+        for (Map.Entry<Integer, Integer> entry : byteCount.entrySet()) {
+            int currentCount = entry.getValue();
+            if (currentCount < minCount) minCount = currentCount;
+        }
+
+        //finds all bytes with value "maxCount"
+        ArrayList<Integer> resultArr = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : byteCount.entrySet()) {
+            if (entry.getValue() == minCount) resultArr.add(entry.getKey());
+        }
+
+        for (int i : resultArr)
+            System.out.print(i + " ");
     }
 }
