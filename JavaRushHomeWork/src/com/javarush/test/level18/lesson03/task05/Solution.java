1f82164ce76767427b1b7813a7601d18fedfba16
VladimirBG <bukharkinv@gmail.com>
2016-10-18 01:16:45 +0300

    level18.lesson03.task05 is ready

--- JavaRushHomeWork/src/com/javarush/test/level18/lesson03/task05/Solution.java
+++ JavaRushHomeWork/src/com/javarush/test/level18/lesson03/task05/Solution.java
@@ -1,25 +1,40 @@
 package com.javarush.test.level18.lesson03.task05;
-
-import java.io.FileInputStream;
-
-
-
-
 /* Сортировка байт
 Ввести с консоли имя файла
 Считать все байты из файла.
 Не учитывая повторений - отсортировать их по байт-коду в возрастающем порядке.
 Вывести на экран
 Закрыть поток ввода-вывода
-
 Пример байт входного файла
 44 83 44
-
 Пример вывода
 44 83
 */
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+
+        FileInputStream inputStream = new FileInputStream(fileName);
+        HashSet<Integer> inputBytes = new HashSet<>();
+
+        while (inputStream.available() > 0) {
+            inputBytes.add(inputStream.read());
+        }
+        inputStream.close();
+        Set<Integer> sortedSet = new TreeSet<Integer>();
+
+        sortedSet.addAll(inputBytes);
+        for (Integer i : sortedSet) {
+            System.out.print(i + " ");
+        }
     }
 }
