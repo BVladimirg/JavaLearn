8bebecb0d998d87963e8376945f847105f1aa722
VladimirBG <bukharkinv@gmail.com>
2016-10-27 14:35:32 +0300

    18.08.t01 18.08.t02 18.08.t03 18.08.t04 18.08.t05

--- JavaRushHomeWork/src/com/javarush/test/testing/Solution.java
+++ JavaRushHomeWork/src/com/javarush/test/testing/Solution.java
@@ -1,16 +1,42 @@
 package com.javarush.test.testing;
 
 
+import java.io.*;
+
 public class Solution
 {
-    public static void main(String[] args) throws InterruptedException
+    public static void main(String[] args) throws IOException
     {
-       Table table = new Table();
-       Table table2 = new Table();
-       Table table3 = new Table();
-       Table table4 = new Table();
-       Table table5 = new Table();
-       Table tabl6e = new Table();
+        //кладем данные в строку
+        StringBuilder sb = new StringBuilder();
+        sb.append("Lena").append('\n');
+        sb.append("Olya").append('\n');
+        sb.append("Anya").append('\n');
+        String data = sb.toString();
+
+        //Оборачиваем строку в класс ByteArrayInputStream
+        InputStream is = new ByteArrayInputStream(data.getBytes());
+
+        //подменяем in
+        System.setIn(is);
+
+        //вызываем обычный метод, который не подозревает о наших манипуляциях
+        readAndPrintLine();
+    }
+
+    public static void readAndPrintLine() throws IOException
+    {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(isr);
+
+        while (true)
+        {
+            String line = reader.readLine();
+            if (line == null) break;
+            System.out.println(line);
+        }
+        reader.close();
+        isr.close();
     }
 }
 
