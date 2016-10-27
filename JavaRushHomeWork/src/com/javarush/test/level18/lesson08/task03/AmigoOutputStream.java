8d99a688fc13a427519a69c3f0c362e95dab3a72
VladimirBG <bukharkinv@gmail.com>
2016-10-28 01:26:31 +0300

    18.08.t01 18.08.t02 18.08.t03 18.08.t04 18.08.t05 completed

--- JavaRushHomeWork/src/com/javarush/test/level18/lesson08/task03/AmigoOutputStream.java
+++ JavaRushHomeWork/src/com/javarush/test/level18/lesson08/task03/AmigoOutputStream.java
@@ -12,12 +12,41 @@ import java.io.IOException;
 2.3 закрыть поток методом close()
 */
 
-public class AmigoOutputStream {
+public class AmigoOutputStream extends FileOutputStream{
     public static String fileName = "C:/tmp/result.txt";
+    private FileOutputStream component;
+
+
+    public AmigoOutputStream(FileOutputStream component) throws FileNotFoundException
+    {
+        super(fileName);
+        this.component = component;
+    }
+
+    @Override
+    public void write(int b) throws IOException
+    {
+        component.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException
+    {
+        component.write(b);
+    }
+
+    public void close() throws IOException
+    {
+        component.flush();
+        component.write("JavaRush © 2012-2013 All rights reserved.".getBytes());
+        component.close();
+
+    }
+
 
     public static void main(String[] args) throws FileNotFoundException {
         new AmigoOutputStream(new FileOutputStream(fileName));
     }
 
-}
+
 }
