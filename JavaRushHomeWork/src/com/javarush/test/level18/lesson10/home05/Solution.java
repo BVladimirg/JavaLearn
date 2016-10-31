51292c29de6f9fac84fb5ce5386241300a95afd2
VladimirBG <bukharkinv@gmail.com>
2016-10-31 17:56:07 +0300

    l18.10.h05 is completed

--- JavaRushHomeWork/src/com/javarush/test/level18/lesson10/home05/Solution.java
+++ JavaRushHomeWork/src/com/javarush/test/level18/lesson10/home05/Solution.java
@@ -14,8 +14,48 @@ package com.javarush.test.level18.lesson10.home05;
 -3.51 - -4
 */
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = reader.readLine();
+        String fileName2 = reader.readLine();
+        reader.close();
+
+        FileInputStream fileInputStream = new FileInputStream(fileName1);
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName2);
+
+        ArrayList<Double> doubles = new ArrayList<>();
+
+        String buff = "";
+        while (fileInputStream.available() > 0) {
+            char nextInputChar = (char) fileInputStream.read();
+            System.out.println(buff);
+            if (nextInputChar == ' ') {
+                doubles.add(Double.parseDouble(buff));
+                buff = "";
+            }
+            buff += nextInputChar;
+        }
+        doubles.add(Double.parseDouble(buff));
+        fileInputStream.close();
+
+        String outputBuffer = "";
+        for (double d : doubles) {
+            int i = (int) Math.round(d);
+            outputBuffer += String.valueOf(i);
+            outputBuffer += " ";
+        }
+
+        byte[] b;
+        b = outputBuffer.getBytes();
+        System.out.println(b);
+
+        fileOutputStream.write(b);
+        fileOutputStream.close();
 
     }
 }
